647553552cce106ecfaa3d1c0fac41ca90340ea8
AK <k311335@gmail.com>
2017-02-22 23:46:34 +0400

    simple_access

---
+++ Access (log, pas).py
@@ -0,0 +1,18 @@
+print ('This learning code access. You can input "Login:admin and Password:123456"')
+print ('##########################################################################')
+while  True:
+    print ('Your login')
+    admin = input ()
+    if admin !='admin':
+        print ('Again')
+        continue
+    break
+print ('Right!')
+while  True:
+    print ('Your password')
+    password = input ()
+    if password !='123456':
+        print ('Again')
+        continue
+    break
+print ('Right! Access open!')
